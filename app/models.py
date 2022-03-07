7818f067818fae60efd34d177a00dd5fb52b4a69
coderboy <coderboy888@gmail.com>
2022-03-07 06:44:31 +0530

    create models.py

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
+from tokenize import group
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+
+class Chat(models.Model):
+    content = models.CharField(max_length=1000)
+    timedtamp = models.DateTimeField(auto_now=True)
+    group = models.ForeignKey('Group',on_delete=models.CASCADE)
+
+class Group(models.Model):
+    name = models.CharField(max_length=225)
